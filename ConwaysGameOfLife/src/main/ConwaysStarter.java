82e93a7ccb199e6395a6ac7022ca9fb93ab02f67
Dodo2314 <superhans12121@web.de>
2017-01-18 00:34:38 +0100

    Added JPanel, added 50x50 cell Array

--- ConwaysGameOfLife/src/main/ConwaysStarter.java
+++ ConwaysGameOfLife/src/main/ConwaysStarter.java
@@ -1,10 +1,19 @@
 package main;
 
+import javax.swing.JFrame;
+
+import window.GameWindow;
+
 public class ConwaysStarter {
 
+	static GameWindow gw = new GameWindow();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame ("Led");
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add (gw);
+        frame.pack();
+        frame.setVisible (true);
 	}
 
 }
--- ConwaysGameOfLife/src/window/GameWindow.java
+++ ConwaysGameOfLife/src/window/GameWindow.java
@@ -1,5 +1,33 @@
 package window;
 
-public class GameWindow {
+import javax.swing.JPanel;
 
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+
+public class GameWindow extends JPanel{
+	
+	private JButton[][] cellArray = new JButton[50][50];
+	
+	
+	public GameWindow() {
+		setupCellArray();
+		setPreferredSize (new Dimension (1200, 1000));
+		setLayout(null);
+	}
+	
+	public void setupCellArray(){
+		int scale = 20;
+		int width = 20;
+		int height = 20;
+		for(int i = 0; i<cellArray.length; i++){
+			for(int r = 0; r<cellArray[i].length; r++){
+				
+				cellArray[i][r] = new JButton("");
+				cellArray[i][r].setBounds(i*scale, r*scale, width, height);
+				add(cellArray[i][r]);
+			}
+		}
+	}
 }
